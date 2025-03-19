bbb6f3e9af3789a51ca6cc6a2f4221af1e4d8e29
gotired <gotired@hotmail.com>
2025-03-19 22:26:23 +0700

    feat: add home page

---
+++ client/pages/index.tsx
@@ -0,0 +1,95 @@
+import MedCard from "@/components/01home/medCard";
+import { useRouter } from "next/router";
+
+const med_items = [
+  "All",
+  "Tablet",
+  "Capsule",
+  "Suppository",
+  "Eyedrops",
+  "Inhaler",
+  "Bottle",
+  "Injectable",
+  "Analgesic",
+  "Antibiotic",
+  "Antidepressant",
+  "Antifungal",
+  "Antihistamine",
+  "Antihypertensive",
+  "Antiviral",
+  "Bronchodilator",
+  "Diuretic",
+  "Sedative",
+];
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div className="h-full p-6 bg-zinc-300 flex flex-col gap-6">
+      <div className="flex justify-between">
+        <h2 className="text-2xl text-black font-semibold">Medicines</h2>
+        <p
+          onClick={() => router.push("/supplies")}
+          className="text-green-500 hover:text-green-400 underline cursor-pointer"
+        >
+          See all
+        </p>
+      </div>
+      <div className="flex gap-4 max-w-[calc(100vw-250px)] overflow-x-auto scrollbar-hide">
+        {med_items.map((item, index) => (
+          <div
+            key={index}
+            className={`py-2 px-4 rounded-4xl ${
+              item === "All" ? "bg-green-500" : "bg-zinc-100 text-zinc-500"
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-4 gap-6">
+        <MedCard
+          name="Beramol"
+          detail="Paracetamol"
+          volume="60 ml"
+          stock={12}
+          price={12.5}
+          unit="bottle"
+        />
+        <MedCard
+          name="Beramol"
+          detail="Paracetamol"
+          volume="60 ml"
+          stock={12}
+          price={12.5}
+          unit="bottle"
+        />
+        <MedCard
+          name="Beramol"
+          detail="Paracetamol"
+          volume="60 ml"
+          stock={12}
+          price={12.5}
+          unit="bottle"
+        />
+        <MedCard
+          name="Beramol"
+          detail="Paracetamol"
+          volume="60 ml"
+          stock={12}
+          price={12.5}
+          unit="bottle"
+        />
+      </div>
+      <div className="flex justify-between">
+        <h2 className="text-2xl text-black font-semibold">Waiting List</h2>
+        <p
+          onClick={() => router.push("/supplies")}
+          className="text-green-500 hover:text-green-400 underline cursor-pointer"
+        >
+          See all
+        </p>
+      </div>
+    </div>
+  );
+}
