a982205b05889afcac515ef2ce5eb645bc9b97a1
gotired <gotired@hotmail.com>
2025-03-26 21:59:27 +0700

    feat: add base app page

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,8 @@
 import type { AppProps } from "next/app";
 import "../app/globals.css";
-import SideBar from "@/components/sidebar";
-import { useRouter } from "next/router";
-import NavBar from "@/components/navbar";
 import { IBM_Plex_Sans_Thai_Looped, Roboto_Flex } from "next/font/google";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
 
 const ibm_plex_sans_thai_loop = IBM_Plex_Sans_Thai_Looped({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
@@ -13,15 +12,20 @@ const ibm_plex_sans_thai_loop = IBM_Plex_Sans_Thai_Looped({
 
 // const roboto = Roboto_Flex({ subsets: ["latin"], variable: "--font-roboto" });
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const path = useRouter();
-  return (
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
     <div className={`h-full flex ${ibm_plex_sans_thai_loop.className}`}>
-      <SideBar pathname={path.pathname} />
-      <div className="flex flex-col w-full">
-        <NavBar />
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </div>
   );
 }
