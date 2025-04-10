089908bd5ed3021cfb43519aacae2055f8f42d3f
gotired <gotired@hotmail.com>
2025-04-10 22:41:33 +0700

    feat: add key in user table

--- backend/internal/domain/user.go
+++ backend/internal/domain/user.go
@@ -4,6 +4,14 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name  string `json:"name"`
-	Email string `json:"email" gorm:"unique"`
+	Name         string `json:"name"`
+	Email        string `json:"email" gorm:"unique"`
+	PasswordHash string `json:"-"`
+	RoleID       uint   `json:"role_id"`
+	Role         Role   `json:"role" gorm:"foreignKey:RoleID;references:ID"`
+}
+
+type UserLogin struct {
+	Email        string
+	PasswordHash string
 }
