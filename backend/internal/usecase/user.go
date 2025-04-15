d5325100edd44601e2a5ed0c44a9448f211721ac
gotired <gotired@hotmail.com>
2025-04-15 14:39:05 +0700

    feat: add validate register detail in user usecase

--- backend/internal/usecase/user.go
+++ backend/internal/usecase/user.go
@@ -5,6 +5,7 @@ import "github.com/gotired/POSPharm/internal/domain"
 type User interface {
 	GetUsers() ([]domain.User, error)
 	GetUser(id uint) (*domain.User, error)
-	RegisterUser(user *domain.User) error
+	RegisterUser(detail domain.UserCreate, passwordHash string) error
 	GetUserLogin(username string) (*domain.UserLogin, error)
+	ValidateRegister(detail domain.UserCreate) error
 }
--- backend/internal/usecase/user_impl.go
+++ backend/internal/usecase/user_impl.go
@@ -1,16 +1,21 @@
 package usecase
 
 import (
+	"errors"
+
 	"github.com/gotired/POSPharm/internal/domain"
 	"github.com/gotired/POSPharm/internal/repository"
+	"gorm.io/gorm"
 )
 
 type userUsecase struct {
-	userRepo repository.User
+	db             *gorm.DB
+	userRepo       repository.User
+	userCredential repository.UserCredential
 }
 
-func NewUser(repo repository.User) User {
-	return &userUsecase{userRepo: repo}
+func NewUser(db *gorm.DB, userRepo repository.User, userCredentialRepo repository.UserCredential) User {
+	return &userUsecase{db: db, userRepo: userRepo, userCredential: userCredentialRepo}
 }
 
 func (u *userUsecase) GetUsers() ([]domain.User, error) {
@@ -21,10 +26,48 @@ func (u *userUsecase) GetUser(id uint) (*domain.User, error) {
 	return u.userRepo.GetByID(id)
 }
 
-func (u *userUsecase) RegisterUser(user *domain.User) error {
-	return u.userRepo.Create(user)
+func (u *userUsecase) RegisterUser(detail domain.UserCreate, passwordHash string) error {
+	return u.db.Transaction(func(tx *gorm.DB) error {
+		userID, err := u.userRepo.Register(tx, detail.FirstName, detail.LastName, detail.Tel)
+		if err != nil {
+			return err
+		}
+		if err = u.userCredential.Register(tx, *userID, detail.UserName, detail.Email, passwordHash); err != nil {
+			return err
+		}
+		return nil
+	})
+
 }
 
 func (u *userUsecase) GetUserLogin(username string) (*domain.UserLogin, error) {
-	return u.userRepo.GetByUsername(username)
+	return u.userCredential.GetIDByUsernameEmail(username, username)
+}
+
+func (u *userUsecase) ValidateRegister(detail domain.UserCreate) error {
+	if detail.Password != detail.ConfirmPassword {
+		return errors.New("password is mismatch")
+	}
+	exists, err := u.userCredential.ValidateEmail(detail.Email)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return errors.New("email is already exists")
+	}
+	exists, err = u.userCredential.ValidateUsername(detail.UserName)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return errors.New("username is already exists")
+	}
+	exists, err = u.userRepo.ValidateTel(detail.Tel)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return errors.New("tel is already exists")
+	}
+	return nil
 }
