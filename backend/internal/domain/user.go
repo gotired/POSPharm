0991e6e3037d26699b36ab85fd635f2e589b7963
gotired <gotired@hotmail.com>
2025-04-15 14:46:23 +0700

    feat: add user domain

--- backend/internal/domain/user.go
+++ backend/internal/domain/user.go
@@ -4,14 +4,42 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name         string `json:"name"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Tel       string `json:"tel" gorm:"unique"`
+}
+
+type UserCredential struct {
+	UserID       uint   `gorm:"not null"`
+	Username     string `json:"username" gorm:"unique"`
 	Email        string `json:"email" gorm:"unique"`
 	PasswordHash string `json:"-"`
-	RoleID       uint   `json:"role_id"`
-	Role         Role   `json:"role" gorm:"foreignKey:RoleID;references:ID"`
+
+	User User `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type UserLogin struct {
+	UserID       uint
 	Email        string
 	PasswordHash string
+}
+
+type UserBranchRole struct {
+	UserID   uint `gorm:"not null"`
+	BranchID uint `gorm:"not null"`
+	RoleID   uint `gorm:"not null"`
+
+	User   User   `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Branch Branch `gorm:"foreignKey:BranchID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Role   Role   `gorm:"foreignKey:RoleID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+type UserCreate struct {
+	UserName        string `json:"username"`
+	FirstName       string `json:"first_name"`
+	LastName        string `json:"last_name"`
+	Email           string `json:"email"`
+	Tel             string `json:"tel"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
 }
